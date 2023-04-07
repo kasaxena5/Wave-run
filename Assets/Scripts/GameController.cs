f130077d719f63506d9412b647399ec0e3860e5e
Kartikeya Saxena <kasaxena@microsoft.com>
2023-04-07 18:31:20 +0530

    Obstacle setup added

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,37 +4,102 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    [SerializeField] private LineController line;
-    private List<Vector2> points;
-    [SerializeField] private float width = 10;
-    [SerializeField] private float stepSize = 1;
-    [SerializeField] private float xOffset = 0;
-    [SerializeField] private float yOffset = 0;
-    [SerializeField] private float scale = 1;
-    [SerializeField] private float heightScale = 3;
-    [SerializeField] private float speed;
+    [SerializeField] private LineController upperLine;
+    [SerializeField] private LineController lowerLine;
+
+    [SerializeField] private ObstacleController obstaclePrefab;
+    private Queue<ObstacleController> obstacles;
+    
+    private List<Vector2> upperLinePoints;
+    private List<Vector2> lowerLinePoints;
+
+    [SerializeField] private float width = 10f;
+    [SerializeField] private float stepSize = 0.5f;
+    
+    [SerializeField] private float upperLineXOffset = 0f;
+    [SerializeField] private float lowerLineXOffset = 10f;
+
+    [SerializeField] private float upperLineHeightOffset = 0f;
+    [SerializeField] private float lowerLineHeightOffset = -1f;
+
+    [SerializeField] private float scale = 1f;
+    [SerializeField] private float heightScale = 5f;
+    [SerializeField] private float speed = 1f;
+
+    [SerializeField] private float obstacleWaitTime = 2;
+
+    private void Awake()
+    {
+        obstacles = new Queue<ObstacleController>();
+    }
 
     void Start()
     {
-        SetupLine();
+        SetupLines();
+        StartCoroutine(SpawnObstacles());
     }
 
-    private void SetupLine()
+    void SetupPlayer()
     {
-        points = new();
-        for (float i = -width; i <= width; i += stepSize)
+
+    }
+
+    IEnumerator SpawnObstacles()
+    {
+        yield return new WaitForSeconds(obstacleWaitTime);
+        while(true)
+        {
+            ObstacleController obstacle = Instantiate(obstaclePrefab);
+            obstacle.position = width + obstacle.width;
+            obstacle.xOffset = upperLineXOffset;
+            obstacle.heightOffset = upperLineHeightOffset;
+            obstacles.Enqueue(obstacle);
+            yield return new WaitForSeconds(obstacleWaitTime);
+        }
+    }
+
+    private void SetupObstacles()
+    {
+        foreach(ObstacleController obstacle in obstacles)
+        {
+            SetupLine(obstacle, upperLineXOffset, obstacle.heightOffset, obstacle.width, obstacle.position);
+            obstacle.position -= (width/scale * Time.deltaTime);
+        }
+
+        if (obstacles.TryPeek(out ObstacleController top))
+        {
+            if (top.position < -width - top.width)
+            {
+                Destroy(top.gameObject);
+                obstacles.Dequeue();
+            }
+        }
+    }
+
+    private void SetupLines()
+    {
+        upperLinePoints = SetupLine(upperLine, upperLineXOffset, upperLineHeightOffset, width);
+        lowerLinePoints = SetupLine(lowerLine, lowerLineXOffset, lowerLineHeightOffset, width);
+        upperLineXOffset += (speed * Time.deltaTime);
+        lowerLineXOffset += (speed * Time.deltaTime);
+    }
+
+    private List<Vector2> SetupLine(LineController line, float xOffset, float heightOffset, float length, float position = 0)
+    {
+        List<Vector2> points = new();
+        for (float i = position - length; i <= position + length + stepSize / 2; i += stepSize)
         {
             float x = (((float)i / (float)width) * scale) + xOffset;
-            float height = Mathf.PerlinNoise(x, yOffset);
-            points.Add(new Vector2(i, heightScale * height));
+            float height = Mathf.PerlinNoise(x, 0);
+            points.Add(new Vector2(i, heightScale * height + heightOffset));
         }
         line.SetupLine(points.ToArray());
+        return points;
     }
 
     void Update()
     {
-        xOffset += (speed * Time.deltaTime);
-        SetupLine();
-
+        SetupLines();
+        SetupObstacles();
     }
 }
---
+++ Assets/Scripts/ObstacleController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleController : LineController
+{
+    public float width;
+    public float position;
+    public float xOffset;
+    public float heightOffset;
+}
